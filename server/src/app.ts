7b2e56fb0faa8d331497f3184bd7d85e305f877c
kaykeeb3 <kaykeloioladev@gmail.com>
2024-05-21 16:52:22 -0300

    feat: main api execution model

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import routes from "./routes";
+import errorHandler from "./utils/ErrorHandler";
 
 dotenv.config();
 
@@ -9,5 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(routes);
+app.use(errorHandler);
 
 export default app;
