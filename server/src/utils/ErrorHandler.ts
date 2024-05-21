618f44a8c20a38c697412e98b6d02148a12fd1ad
kaykeeb3 <kaykeloioladev@gmail.com>
2024-05-21 16:51:43 -0300

    fix: config validations - error

---
+++ server/src/utils/ErrorHandler.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
+};
+
+export default errorHandler;
