06d2b535519737e1f14ffe322fcdda094c5b5995
kaykeeb3 <kaykeloioladev@gmail.com>
2024-05-21 17:10:39 -0300

    fix: add new route (home)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import AuthController from "./controllers/AuthController";
 import UserController from "./controllers/UserController";
+import HomeController from "./controllers/HomeController";
+import authenticateToken from "./middlewares/authenticateToken";
 
 const routes = Router();
 const authController = new AuthController();
 const userController = new UserController();
-
+const homeController = new HomeController();
 routes.post("/login", authController.login);
 routes.post("/register", userController.register);
+routes.get("/home", authenticateToken, homeController.home);
 
 export default routes;
