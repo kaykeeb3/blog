c0d549cdb846d7fe4d6c44e7c4953d28b14fe725
kaykeeb3 <kaykeloioladev@gmail.com>
2024-05-22 17:49:25 -0300

    feat: add new routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,18 +1,19 @@
-// Em src/routes.ts
-
 import { Router } from "express";
 import AuthController from "./controllers/AuthController";
 import UserController from "./controllers/UserController";
 import HomeController from "./controllers/HomeController";
 import NewsController from "./controllers/NewsController";
+import ProfileController from "./controllers/ProfileController";
 import authenticateToken from "./middlewares/authenticateToken";
 import validateNews from "./middlewares/validateNews";
+import { validateProfile } from "./middlewares/validateProfile";
 
 const routes = Router();
 const authController = new AuthController();
 const userController = new UserController();
 const homeController = new HomeController();
 const newsController = new NewsController();
+const profileController = new ProfileController();
 
 routes.post("/login", authController.login);
 routes.post("/register", userController.register);
@@ -21,5 +22,14 @@ routes.post("/news", authenticateToken, validateNews, newsController.create);
 routes.put("/news/:id", authenticateToken, validateNews, newsController.update);
 routes.delete("/news/:id", authenticateToken, newsController.delete);
 routes.get("/news", authenticateToken, newsController.getAll);
+
+// Rotas de perfil
+routes.post(
+  "/profile",
+  authenticateToken,
+  validateProfile,
+  profileController.createOrUpdate
+);
+routes.get("/profile", authenticateToken, profileController.getProfile);
 
 export default routes;
